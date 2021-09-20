211c90d4a948208282076cf5e591db747b92a3d4
Gil Tene <gil@cloud7.com>
2021-09-20 07:14:47 -0700

    Initial commit

---
+++ src/main/java/GuavaArray.java
@@ -0,0 +1,6 @@
+import com.google.common.util.concurrent.AtomicDoubleArray;
+public class GuavaArray extends AtomicDoubleArray {
+    public GuavaArray(int length) {
+        super(length);
+    }
+}
